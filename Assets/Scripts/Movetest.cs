6a0f665211c7c961919baf9cb5ccf6a7dcc67e80
insimotiwala <86950756+insimotiwala@users.noreply.github.com>
2021-10-16 16:16:39 -0700

    initial commit

---
+++ Assets/Scripts/Movetest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movetest : MonoBehaviour
+{
+    // public GameObject Factory;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        /*if (Input.GetKey(KeyCode.Mouse0))
+        {
+        else
+            { return; }
+        }*/
+    }
+}
